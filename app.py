6f731d6b08705fe65bcf5d6d5c53b4296b2a23f9
nirstore <com@nir.org.il>
2025-06-15 16:28:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,7 @@ def get_folder_size(path='.'):
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
