95a131a73e0fadb57a5a816a801020952d88bc4f
nirstore <com@nir.org.il>
2025-06-16 16:57:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,7 @@ def send_reset_email(to_email, temp_password):
         server.send_message(msg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+  
